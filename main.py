7a85241e89b3f7e9a3d0025d59a85100fb20f841
jasond-osc <102763865+jasond-osc@users.noreply.github.com>
2022-04-01 10:06:00 +1300

    Create GUI

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-#Starting multi_choice 
+from tkinter import *
+
+class CatGUI:
+    def __init__(self, parent):
+        self.result = StringVar()
+        self.result.set("") 
+
+        f1 = Frame(parent)
+
+        Label(f1, text="Question").grid(column=0)
+        Label(f1, text="What is the capital of New Zealand").grid(column=1, row=0)
+
+        adjective_list = ["Wellington", "Hamilton", "Auckland", "Christchurch"]
+
+        r = 1
+        for i in(adjective_list):
+            Radiobutton(f1, text=i, value=i, variable=self.result, command=self.display_choice).grid(column=1, row=r, sticky=W)
+            r += 1
+
+        f1.pack()
+
+        self.output_label = Label(parent, text=self.result.get())
+        self.output_label.pack()
+
+    def display_choice(self):
+        return
+    
+if __name__ == "__main__":
+    root = Tk()
+    buttons = CatGUI(root)
+    root.mainloop()
