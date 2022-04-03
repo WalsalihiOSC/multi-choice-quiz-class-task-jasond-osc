715a6311588e7b22e64794422f61208c7445c88a
jasond-osc <102763865+jasond-osc@users.noreply.github.com>
2022-04-03 20:48:02 +1200

    Add class for data

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from tkinter import *
 
-class CatGUI:
+class Cities:
+    def __init__ (self):
+        self.cities = ["Wellington", "Hamilton", "Auckland", "Christchurch"]
+
+    def get_cities(self):
+        return self.cities
+
+    def capital_city(self) :
+        return "Wellington"
+
+class GUI:
     def __init__(self, parent):
         self.result = StringVar()
         self.result.set("") 
@@ -9,11 +19,11 @@ class CatGUI:
 
         Label(f1, text="Question").grid(column=0)
         Label(f1, text="What is the capital of New Zealand").grid(column=1, row=0)
-
-        adjective_list = ["Wellington", "Hamilton", "Auckland", "Christchurch"]
-
+        self.c = Cities()
+        cities = self.c.get_cities()
+        
         r = 1
-        for i in(adjective_list):
+        for i in(cities):
             Radiobutton(f1, text=i, value=i, variable=self.result, command=self.display_result).grid(column=1, row=r, sticky=W)
             r += 1
 
@@ -23,12 +33,12 @@ class CatGUI:
         self.output_label.pack()
 
     def display_result(self):
-        if self.result.get() == "Wellington":
+        if self.result.get() == self.c.capital_city():
             self.output_label.configure(text="Correct")
         else:
             self.output_label.configure(text="Incorrect")
     
 if __name__ == "__main__":
     root = Tk()
-    buttons = CatGUI(root)
+    buttons = GUI(root)
     root.mainloop()
